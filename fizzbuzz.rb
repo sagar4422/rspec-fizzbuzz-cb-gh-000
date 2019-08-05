90fb95a9e66a64c8e707bde0e98d1a47104b7e90
Sagar Gururaj Deshpande <sagargururaj.d@gmail.com>
2019-08-05 14:02:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if number%3 == 0
+    puts "fizz"
+  end
+  elsif number%5 == 0
+    puts "buzz"
+  end
+  elsif (number%3 && number%5) == 0
+    puts "fizzbuzz"
+  end
+  else
+  end
+end
+
+fizzbuzz(3)
